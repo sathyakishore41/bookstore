45096302ded9a483320d8d0bae4e9eb2b4b4a5ec
Akanksha2307 <akanksharao2307@gmail.com>
2024-06-08 14:21:57 +0530

    1st commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// Function to show book summary in modal dialog
+function showBookSummary() {
+    // Get the modal
+    var modal = document.getElementById("summaryModal");
+
+    // Display the modal
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+    // Get the modal
+    var modal = document.getElementById("summaryModal");
+
+    // Hide the modal
+    modal.style.display = "none";
+}
